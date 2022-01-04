cf2b51a8ab9b29e414bc7612adc68771a090bb2f
Fakhri Akbar <fakhriakbr@gmail.com>
2022-01-04 12:03:18 +0700

    attempt: use db to store temporary data

---
+++ modules/db.js
@@ -0,0 +1,51 @@
+// define dependencies
+const { Pool } = require('pg');
+
+// create connection
+const pool = new Pool({
+    user: 'mmlejmywrivqjm',
+    host: 'ec2-52-200-188-218.compute-1.amazonaws.com',
+    database: 'd6bj1259frb0eb',
+    password: '243b7172d72552f38aa14fe01d729387297960a01ed5ebf143ed7a81aea261e2',
+    port: 5432,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+
+// update record
+exports.update = function(key, value) {
+    pool.query("UPDATE sfmc_logs SET value = '" + value + "' WHERE key = '" + key + "'", (err, res) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(key + ' successfully updated.');
+        };
+    });
+};
+
+// get access_token
+exports.getToken = function() {
+    return new Promise (function(resolve, reject) {
+        pool.query("SELECT value FROM sfmc_logs WHERE key = 'access_token'", (err, res) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(res.rows[0].value);
+            };
+        });
+    });
+}
+
+// auto increment for callback ID
+exports.getStatusCallbackID = function() {
+    return new Promise (function(resolve, reject) {
+        pool.query("SELECT value FROM sfmc_logs WHERE key = 'status_callback_id'", (err, res) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(parseInt(res.rows[0].value) + 1);
+            };
+        });
+    });
+};
---
+++ modules/de.js
@@ -0,0 +1,72 @@
+// define dependencies
+var axios = require('axios');
+var db = require('./db');
+
+// define variables
+const auth_url = 'https://mcx3dk6gqx05byn626r3yqc9-hl0.auth.marketingcloudapis.com';
+const rest_instance_url = 'https://mcx3dk6gqx05byn626r3yqc9-hl0.rest.marketingcloudapis.com/';
+
+// request new access_token
+async function renewToken() {
+    return axios.post(auth_url + '/v2/token', {
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        grant_type: 'client_credentials',
+        client_id: 'u5tcbvwgpy4b4a6wefyf1jij',
+        client_secret: 'SowaSYrKPmM7ZLzEBE3A0DBc',
+        account_id: '110006474'
+    })
+    .then(success => {
+        // update access_token to database
+        db.update('access_token', success.data.access_token);
+
+        return {status: 200, body: success};
+    })
+    .catch(error => {
+        return {status: 400, body: error};
+    });
+};
+
+// insert to data extension
+exports.insert = async function insert(data, externalKeyDE) {
+    let result;
+    let access_token = await db.getToken();
+    let newToken;
+
+    await axios.post(rest_instance_url + 'data/v1/async/dataextensions/key:' + externalKeyDE + '/rows',    
+            {
+                items: [data]
+            },
+            {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + access_token
+                }
+            }
+        )
+        .then(success => {
+            result = {status: 200, body: success};
+        })
+        .catch(error => {
+            result = {status: 400, body: error};
+        });
+    
+    if (result.status === 400) {
+        if (result.body.response.status === 401) {
+            newToken = await renewToken();
+
+            if (newToken.status === 200) {
+                console.log(newToken.body.data.access_token);
+                insert(data, externalKeyDE);
+                return newToken.body.data.access_token;
+            } else {
+                console.log('Failed to get access_token.');
+                return 'Failed to get access_token.';
+            };
+        };
+    };
+
+    console.log(result);
+    return result;
+};
