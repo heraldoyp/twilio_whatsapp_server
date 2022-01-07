db8c3ff578d36e58ab57cd0eb9dec0b746c93ead
ingloo <heraldo@blendmedia.co.id>
2022-01-07 04:01:07 -0800

    #1 Attempt: add to heroku

--- routes/whatsapp.js
+++ routes/whatsapp.js
@@ -1,7 +1,9 @@
 var whatsapp = require('../modules/whatsapp');
 
 exports.send = function(req, res) {
-
+    
+    console.log(req.body.inArguments[0]);
+    res.send("ini dari whastapp send")
 };
 
 exports.callback = function(req, res) {
