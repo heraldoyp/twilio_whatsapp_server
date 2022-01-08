a7567e3b9acf328276be00afc755d2d92cd8d97b
ingloo <heraldo@blendmedia.co.id>
2022-01-07 20:48:46 -0800

    #7 Attempt: Send whatsapp ready

--- modules/whatsapp.js
+++ modules/whatsapp.js
@@ -3,37 +3,26 @@ var axios = require('axios');
 var db = require('./db');
 var de = require('./de');
 var twilio = require('twilio');
-
-// var data = {
-//     "Message": "Hello ini hardcode dari server-side",
-//     "Sender": "14155238886",
-//     "PhoneNumber": "6285719752942"
-// }
+//require('dotenv').config({ path: '../.env'}); // Uninstall sebelum publish
 
 // Send Whatsapp Message
 exports.send = async function(data) {
     const accountSid = process.env.TWILIO_ACCOUNT_SID; 
     const authToken = process.env.TWILIO_AUTH_TOKEN; 
-    //const client = twilio(accountSid, authToken); 
- 
-    
-    // client.messages 
-    // .create({ 
-    //     body: data.Message, 
-    //     from: 'whatsapp:+'+data.Sender,       
-    //     to: 'whatsapp:+'+data.PhoneNumber
-    // }) 
-    // .then(response => {
-    //     console.log(response);
-    //     return {status: 200, body: response};
-    // }) 
-    // .catch(error => {
-    //     console.log(error)
-    //     return {status: 400, body: error};
-    // });
-    console.log("Account SID :"+accountSid);
-    console.log("Auth Token :"+authToken);
-    console.log(JSON.stringify(data));
-};
+    const client = twilio(accountSid, authToken); 
 
-// console.log(JSON.stringify(this.send(data)));
+    client.messages 
+    .create({ 
+        body: data.Message, 
+        from: 'whatsapp:+'+data.Sender,       
+        to: 'whatsapp:+'+data.PhoneNumber
+    }) 
+    .then(response => {
+        console.log(response);
+        return {status: 200, body: response};
+    }) 
+    .catch(error => {
+        console.log(error)
+        return {status: 400, body: error};
+    });
+};
