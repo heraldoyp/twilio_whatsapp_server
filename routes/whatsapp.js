29904d0ed16796d19c709c7f600ef79751279bb0
ingloo <heraldo@blendmedia.co.id>
2022-01-07 20:50:25 -0800

    #8 Attempt: reconfigure send whatsapp response

--- routes/whatsapp.js
+++ routes/whatsapp.js
@@ -6,40 +6,40 @@ exports.execute = async function (req, res) {
     let waRequestBody = {
         body: req.body.inArguments[0]
     }
+
     console.log(util.inspect(waRequestBody));
-    res.status(200).send("Execute Success")
-    //var sendWhatsapp = await whatsapp.send(waRequestBody);
+    var sendWhatsapp = await whatsapp.send(waRequestBody);
 
-    // if(sendWhatsapp.status == 200){
-    //     waResponseBody = {
-    //         "sid": sendWhatsapp.sid,
-    //         "date_created": sendWhatsapp.date_created,
-    //         "date_updated": sendWhatsapp.date_updated,
-    //         "date_sent": sendWhatsapp.date_sent,
-    //         "account_sid": sendWhatsapp.account_sid,
-    //         "to": sendWhatsapp.to,
-    //         "from": sendWhatsapp.from,
-    //         "messaging_service_sid": sendWhatsapp.messaging_service_sid,
-    //         "body": sendWhatsapp.body,
-    //         "status": sendWhatsapp.status,
-    //         "num_segments": sendWhatsapp.num_segments,
-    //         "num_media": sendWhatsapp.num_media,
-    //         "direction": sendWhatsapp.direction,
-    //         "api_version": sendWhatsapp.api_version,
-    //         "price": sendWhatsapp.price,
-    //         "price_unit": sendWhatsapp.price_unit,
-    //         "error_code": sendWhatsapp.error_code,
-    //         "error_message": sendWhatsapp.error_message,
-    //         "uri": sendWhatsapp.uri,
-    //         "subresource_uris": JSON.stringify(sendWhatsapp.subresource_uris)
-    //     }
+    if(sendWhatsapp.status == 200){
+        waResponseBody = {
+            "sid": sendWhatsapp.sid,
+            "date_created": sendWhatsapp.date_created,
+            "date_updated": sendWhatsapp.date_updated,
+            "date_sent": sendWhatsapp.date_sent,
+            "account_sid": sendWhatsapp.account_sid,
+            "to": sendWhatsapp.to,
+            "from": sendWhatsapp.from,
+            "messaging_service_sid": sendWhatsapp.messaging_service_sid,
+            "body": sendWhatsapp.body,
+            "status": sendWhatsapp.status,
+            "num_segments": sendWhatsapp.num_segments,
+            "num_media": sendWhatsapp.num_media,
+            "direction": sendWhatsapp.direction,
+            "api_version": sendWhatsapp.api_version,
+            "price": sendWhatsapp.price,
+            "price_unit": sendWhatsapp.price_unit,
+            "error_code": sendWhatsapp.error_code,
+            "error_message": sendWhatsapp.error_message,
+            "uri": sendWhatsapp.uri,
+            "subresource_uris": JSON.stringify(sendWhatsapp.subresource_uris)
+        }
 
-    //     console.log(waResponseBody)
-    //     logData(req);
-    //     res.status(200).send("Execute");    
-    // }else{
-    //     res.status(400).send("Execute Error");   
-    // }
+        console.log(util.inspect(waResponseBody));
+        logData(req);
+        res.status(200).send("Execute");    
+    }else{
+        res.status(400).send("Execute Error");   
+    }
 }
 
 
